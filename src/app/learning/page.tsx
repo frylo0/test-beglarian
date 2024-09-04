14263998df98c493baa900fb0927c4b3a9e1dac7
frylo0 <frylo.d.ts@gmail.com>
2024-09-04 23:25:50 +0300

    feat: layout index page

--- src/app/learning/page.tsx
+++ src/app/learning/page.tsx
@@ -1,12 +1,11 @@
 import cn from 'clsx';
 
-import { Button } from '@/components/common/Button/Button';
 import { sButton, sMain } from './page.css';
 
 export default function Home() {
 	return (
 		<main className={cn(sMain)}>
-			<Button className={cn(sButton)}>Click me</Button>
+			<button className={cn(sButton)}>Click me</button>
 		</main>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,193 @@
 'use client';
 
 import cn from 'clsx';
-import Image from 'next/image';
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
+import { CSSProperties, ReactNode } from 'react';
 
-import SVG_Next from '@/assets/vector/next.svg?url';
-import SVG_Vercel from '@/assets/vector/vercel.svg';
-import { useApp$ } from '@/stores/app.store';
+import SVG_Book from '@/assets/vector/book.svg';
+import SVG_Bookmark from '@/assets/vector/bookmark.svg';
+import SVG_Chart from '@/assets/vector/chart.svg';
+import SVG_Chevron from '@/assets/vector/chevron.svg';
+import SVG_Crown from '@/assets/vector/crown.svg';
+import SVG_Fire from '@/assets/vector/fire.svg';
+import SVG_Gamepad from '@/assets/vector/gamepad.svg';
+import SVG_Graduation from '@/assets/vector/graduation.svg';
+import SVG_Learn from '@/assets/vector/learn.svg';
+import SVG_Pen from '@/assets/vector/pen.svg';
+import SVG_Rocket from '@/assets/vector/rocket.svg';
+import SVG_Sign from '@/assets/vector/sign.svg';
+import SVG_TvPlay from '@/assets/vector/tv-play.svg';
+import SVG_Home from '@/assets/vector/home.svg';
+import SVG_Route from '@/assets/vector/route.svg';
+import SVG_Rank from '@/assets/vector/rank.svg';
+import SVG_Profile from '@/assets/vector/profile.svg';
+import { Adaptive } from '@/components/common/Adaptive/Adaptive';
 import {
+	sArrow,
+	sBadge,
+	sBadges,
+	sBar,
 	sCard,
-	sCardH2,
-	sCardP,
-	sCenter,
-	sCode,
-	sCounter,
-	sDescription,
-	sDescriptionA,
-	sDescriptionP,
-	sGrid,
-	sLogo,
+	sCardIcon,
+	sCardItem,
+	sCards,
+	sCardTitle,
+	sCategory,
+	sCurrentTest,
+	sDash,
+	sDashEnabled,
+	sDevice,
+	sHeader,
+	sHIcon,
+	sHighlight,
+	sHighlights,
+	sHTitle,
+	sIcon,
+	sInfo,
+	sLabel,
 	sMain,
-	sMainA,
-	sVercelLogo,
+	sNavActive,
+	sNavbar,
+	sNavIcon,
+	sNavItem,
+	sNavLink,
+	sNavList,
+	sNavMenu,
+	sNavTitle,
+	sProgress,
+	sText,
+	sTitle,
 } from './page.css';
 
 export default function Page() {
-	const { count, increment, decrement } = useApp$();
+	const dashes = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+	const passedCount = dashes.filter((item) => !!item).length;
+	const totalCount = dashes.length;
 
 	return (
-		<main className={cn(sMain)}>
-			<div className={cn(sDescription)}>
-				<p className={cn(sDescriptionP)}>
-					Get started by editing&nbsp;
-					<code className={cn(sCode)}>src/app/page.tsx</code>
-				</p>
-				<div>
-					<Link
-						className={cn(sDescriptionA, sMainA)}
-						href="https://vercel.com?utm_source=typescript-nextjs-starter"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						By <SVG_Vercel className={cn(sVercelLogo)} width={100} height={24} />
-					</Link>
-				</div>
-			</div>
+		<main className={cn(sMain, sDevice)}>
+			<Adaptive>
+				<header className={cn(sHeader)}>
+					<h1 className={cn(sLabel)}>Ray Driving Theory</h1>
+					<div className={cn(sBadges)}>
+						<div className={cn(sBadge)}>
+							<SVG_Crown />
+						</div>
+						<div className={cn(sBadge)}>
+							<SVG_Fire /> 13
+						</div>
+					</div>
+				</header>
 
-			<div className={cn(sCenter)}>
-				<Image className={cn(sLogo)} src={SVG_Next.src} alt="Next.js Logo" width={180} height={37} priority />
+				<Link href="/learning" className={cn(sCurrentTest, sCard)}>
+					<div className={cn(sInfo)}>
+						<figure className={cn(sIcon)}>
+							<SVG_Graduation />
+						</figure>
 
-				<div className={cn(sCounter)}>
-					<button onClick={decrement}>-</button>
-					{count}
-					<button onClick={increment}>+</button>
-				</div>
-			</div>
+						<h2 className={cn(sTitle)}>Learning</h2>
+						<p className={cn(sCategory)}>Category B</p>
 
-			<div className={cn(sGrid)}>
-				<Link href="/other-page" className={cn(sCard, sMainA)} target="_self" rel="noopener noreferrer">
-					<h2 className={cn(sCardH2)}>Link to &quot;other page&quot;</h2>
-					<p className={cn(sCardP)}>Check some examples on this page.</p>
+						<button className={cn(sArrow)}>
+							<SVG_Chevron />
+						</button>
+					</div>
+
+					<div className={cn(sProgress)}>
+						<p className={cn(sText)}>
+							{passedCount} questions out of {totalCount} passed
+						</p>
+						<div className={cn(sBar)}>
+							{dashes.map((enabled, i) => (
+								<div className={cn(sDash, enabled && sDashEnabled)} key={i} />
+							))}
+						</div>
+					</div>
 				</Link>
-			</div>
+
+				<div className={cn(sCards)}>
+					<Card icon={<SVG_Learn />} title="DVSA Exam" href="#" iconColor="#FF2045" />
+					<Card icon={<SVG_Rocket />} title="Express mode" href="#" />
+					<Card icon={<SVG_TvPlay />} title="Hazard perception" href="#" />
+					<Card icon={<SVG_Sign />} title="Road signs" href="#" />
+					<Card icon={<SVG_Gamepad />} title="Multiplayer Learning" href="#" />
+				</div>
+
+				<div className={cn(sHighlights)}>
+					<Highlight icon={<SVG_Pen />} title="Mistakes" href="#" />
+					<Highlight icon={<SVG_Chart />} title="Statistics" href="#" />
+					<Highlight icon={<SVG_Book />} title="Highway Code" href="#" />
+					<Highlight icon={<SVG_Bookmark />} title="Favorites" href="#" />
+				</div>
+			</Adaptive>
+
+			<nav className={cn(sNavbar, sDevice)}>
+				<menu className={cn(sNavMenu)}>
+					<ul className={cn(sNavList)}>
+						<NavItem href="#" icon={<SVG_Home />} title="Home" active />
+						<NavItem href="#" icon={<SVG_Route />} title="Your plan" />
+						<NavItem href="#" icon={<SVG_Rank />} title="Leaderboard" />
+						<NavItem href="#" icon={<SVG_Profile />} title="Profile" />
+					</ul>
+				</menu>
+			</nav>
 		</main>
 	);
 }
+
+interface CardProps {
+	className?: string;
+	href: LinkProps['href'];
+	icon: ReactNode;
+	iconColor?: CSSProperties['color'];
+	title: ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({ className, href, title, icon, iconColor = '#6B2ABC' }) => {
+	const cssVars = {
+		'--icon-color': iconColor,
+	} as CSSProperties;
+
+	return (
+		<Link className={cn(sCardItem, sCard, className)} style={cssVars} href={href}>
+			<figure className={cn(sCardIcon)}>{icon}</figure>
+			<p className={cn(sCardTitle)}>{title}</p>
+		</Link>
+	);
+};
+
+interface HighlightProps {
+	className?: string;
+	title: ReactNode;
+	icon: ReactNode;
+	href: LinkProps['href'];
+}
+
+const Highlight: React.FC<HighlightProps> = ({ className, href, title, icon }) => {
+	return (
+		<Link href={href} className={cn(sHighlight, className)}>
+			<figure className={cn(sHIcon)}>{icon}</figure>
+			<p className={cn(sHTitle)}>{title}</p>
+		</Link>
+	);
+};
+
+interface NavItemProps {
+	className?: string;
+	title: ReactNode;
+	icon: ReactNode;
+	href: LinkProps['href'];
+	active?: boolean;
+}
+
+const NavItem: React.FC<NavItemProps> = ({ className, title, icon, href, active }) => {
+	return (
+		<li className={cn(sNavItem, active && sNavActive, className)}>
+			<Link href={href} className={cn(sNavLink)}>
+				<figure className={cn(sNavIcon)}>{icon}</figure>
+				<p className={cn(sNavTitle)}>{title}</p>
+			</Link>
+		</li>
+	);
+};
